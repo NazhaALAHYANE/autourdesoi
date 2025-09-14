553c408584424519a2e0c30f06436b18d0df66ab
nazha75 <nazhaalahyane@gmail.com>
2025-09-14 19:29:53 +0200

    Feature: Add Composteur model and register in admin

--- autourdesoietaudela/Goeco/admin.py
+++ autourdesoietaudela/Goeco/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Composteur
 
 # Register your models here.
+
+@admin.register(Composteur)
+class ComposteurAdmin(admin.ModelAdmin):
+    list_display = ("nom", "entite", "adresse")
+    search_fields = ("nom", "entite", "adresse")
--- autourdesoietaudela/Goeco/models.py
+++ autourdesoietaudela/Goeco/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Composteur(models.Model):
+    nom= models.CharField(max_length=200, verbose_name="Nom du site")
+    entite= models.CharField(max_length=200, verbose_name="Entité en charge")
+    adresse= models.CharField(max_length=300, verbose_name="Adresse")
+    arrondissement= models.CharField(max_length=10, blank=True, null=True, verbose_name="Arrodissement")
+
+    def __str__(self):
+        return self.nom

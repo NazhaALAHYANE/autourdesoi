81b751610eaab07f14dd5faec4e59a7079dd920c
nazha75 <nazhaalahyane@gmail.com>
2025-09-16 17:41:05 +0200

    Feature: add recyclerie/ressourcerie and register in admin

--- autourdesoietaudela/Goeco/admin.py
+++ autourdesoietaudela/Goeco/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Composteur
+from .models import RecyclerieRessourcerie
 
 # Register your models here.
 
@@ -7,3 +8,9 @@ from .models import Composteur
 class ComposteurAdmin(admin.ModelAdmin):
     list_display = ("nom", "entite", "adresse")
     search_fields = ("nom", "entite", "adresse")
+
+
+@admin.register(RecyclerieRessourcerie)
+class RecyclerieRessourcerie(admin.ModelAdmin):
+    list_display = ("operateur", "adresse", "code_postal")
+    search_fields = ("operateur", "adresse", "code_postal")
--- autourdesoietaudela/Goeco/models.py
+++ autourdesoietaudela/Goeco/models.py
@@ -10,3 +10,15 @@ class Composteur(models.Model):
 
     def __str__(self):
         return self.nom
+
+class RecyclerieRessourcerie(models.Model):
+    operateur= models.CharField(max_length=255, verbose_name="Opérateur")
+    adresse= models.CharField(max_length=255, verbose_name="Adresse")
+    code_postal= models.CharField(max_length=10, verbose_name="Code postal")
+
+    class Meta:
+        verbose_name= "Recyclerie/ Ressourcerie/ Point d'apport volontaire"
+        verbose_name_plural= "Recycleries/ Ressourceries/ Points d'apport volontaire"
+    
+    def __str__(self):
+        return f"{self.operateur} - {self.adresse} ({self.code_postal})"

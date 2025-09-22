d6d37beabd55e0b4993dedcedb570d1ba0e3ae3e
nazha75 <nazhaalahyane@gmail.com>
2025-09-22 20:12:06 +0200

    fix: problem of cache to display css file

--- autourdesoietaudela/Goeco/views.py
+++ autourdesoietaudela/Goeco/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.utils.timezone import now
 # Create your views here.
+
+def home(request):
+    return render(request, 'Goeco/base.html', {'timestamp': int(now().timestamp())})

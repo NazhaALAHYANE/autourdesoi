f4ae344db5ee517c60b95e6eea3c4fbb0ccc8994
nazha75 <nazhaalahyane@gmail.com>
2025-09-29 20:43:21 +0200

    Add html and css to composteur template and complete views file

--- autourdesoietaudela/Goeco/urls.py
+++ autourdesoietaudela/Goeco/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("composteur_page/", views.composteur_page, name="composteurs"),
 ]
--- autourdesoietaudela/Goeco/views.py
+++ autourdesoietaudela/Goeco/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.utils.timezone import now
+from .models import Composteur, RecyclerieRessourcerie
+
 # Create your views here.
 
 def home(request):
     return render(request, 'Goeco/base.html', {'timestamp': int(now().timestamp())})
+
+def composteur_page(request):
+    composteurs = Composteur.objects.all()
+    context = {
+        'composteurs': composteurs,
+        'timestamp': int(now().timestamp()),
+    }
+    return render(request, 'Goeco/composteur_page.html', context)

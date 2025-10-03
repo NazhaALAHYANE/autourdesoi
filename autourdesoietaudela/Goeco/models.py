a5104821b0e4474d57845b01782dbeacd60b25ca
nazha75 <nazhaalahyane@gmail.com>
2025-10-03 19:06:03 +0200

    fix: update model fields with options (blank...etc)

--- autourdesoietaudela/Goeco/models.py
+++ autourdesoietaudela/Goeco/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Composteur(models.Model):
     nom= models.CharField(max_length=200, verbose_name="Nom du site")
     entite= models.CharField(max_length=200, verbose_name="Entité en charge")
-    adresse= models.CharField(max_length=300, verbose_name="Adresse")
+    adresse= models.CharField(max_length=300, blank=True, null=True, verbose_name="Adresse")
     arrondissement= models.CharField(max_length=10, blank=True, null=True, verbose_name="Arrodissement")
 
     def __str__(self):
